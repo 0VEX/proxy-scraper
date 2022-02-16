07ff3ca0e123a8ffe5087582d980b1bbc6f792a7
vexdes1 <97989102+vexdes1@users.noreply.github.com>
2022-02-16 10:57:52 +0300

    added extre info about the tool and api

--- proxy-scraper.py
+++ proxy-scraper.py
@@ -39,35 +39,95 @@ print(Fore.RED+"""
 # first msg
 
 ready = input (f'{Fore.RED} [!] {Fore.GREEN} this program will autoscrape (HTTP,SOCKS4,SOCKS5) proxies into separate files (Hit "ENTER" To countinue)')
-
+# ------------------------------------------------------------------
 # -http- proxies
 
+# api
 url = 'https://api.openproxylist.xyz/http.txt'
+
+# request the proxies from the api
+
 r = requests.get(url)
+
+# copy the proxies
+
 result = r.text
+
+# create a file in your pc with the path the tool is specified
+
 with open ("http.txt", "w") as file:
+           
+# paste the proxies and save them           
+           
     file.write(result)
+
+# ------------------------------------------------------------------
+
 
 # -https- proxies [not working]
 
 #url = 'https://api.openproxylist.xyz/https.txt'
+
+
+
 #r = requests.get(url)
+
+
+
 #result = r.text
+
+
+
 #with open ("https.txt", "w") as file:
+
+# paste the proxies and save them
+
 #    file.write(result)
+
+# ------------------------------------------------------------------
 
 # -socks4- proxies
 
+# api
+
 url = 'https://api.openproxylist.xyz/socks4.txt'
+
+# request the proxies from the api
+
 r = requests.get(url)
+
+# copy the proxies
+
 result = r.text
+
+# create a file in your pc with the path the tool is specified
+
 with open ("socks4.txt", "w") as file:
+           
+# paste the proxies and save them
+           
     file.write(result)
+
+# ------------------------------------------------------------------
 
 # -socks5- proxies
 
+# api
+
 url = 'https://api.openproxylist.xyz/socks5.txt'
+
+# request the proxies from the api
+
 r = requests.get(url)
+
+# copy the proxies
+
 result = r.text
+
+# create a file in your pc with the path the tool is specified
+
 with open ("socks5.txt", "w") as file:
+           
+# paste the proxies and save them
+           
     file.write(result)
